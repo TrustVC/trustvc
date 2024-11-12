ab0a5e9fa1ac2b9c55e00dd1d6127aa83f6236a5
nghaninn <43451336+nghaninn@users.noreply.github.com>
2024-11-12 12:48:15 +0800

    chore: fix test

--- src/__tests__/w3c/verify.test.ts
+++ src/__tests__/w3c/verify.test.ts
@@ -4,13 +4,13 @@ import { W3C_VERIFIABLE_DOCUMENT } from '../fixtures/fixtures';
 
 describe.concurrent('W3C verify', () => {
   it('should verify a valid document', async ({ expect }) => {
-    const verificationResult = await verifyW3CSignature(W3C_VERIFIABLE_DOCUMENT as any);
+    const verificationResult = await verifyW3CSignature(W3C_VERIFIABLE_DOCUMENT);
     expect(verificationResult.verified).toBe(true);
   });
 
   it('should verify a tampered document', async ({ expect }) => {
     const tampered = { ...W3C_VERIFIABLE_DOCUMENT, expirationDate: '2029-12-03T12:19:53Z' };
-    const verificationResult = await verifyW3CSignature(tampered as any);
+    const verificationResult = await verifyW3CSignature(tampered);
     expect(verificationResult.verified).toBe(false);
   });
 });
