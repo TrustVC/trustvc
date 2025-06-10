3ccb21ae3faa7dc9336d3d871d0db35cd5462d76
nghaninn <43451336+nghaninn@users.noreply.github.com>
2025-06-10 17:10:02 +0800

    fix: remove duplicate condition

--- src/utils/ethers/index.ts
+++ src/utils/ethers/index.ts
@@ -7,12 +7,12 @@ export const isV6EthersProvider = (provider: any): boolean => {
   // if ((provider as ethers.providers.Provider)._isProvider === true) {
   if (provider?._isProvider === true) {
     return false;
-  } else if (provider?._isSigner === true && provider?.provider && provider?.signTransaction) {
-    // } else if ((provider as ethers.Signer)._isSigner === true && (provider as ethers.Signer)?.provider && (provider as ethers.Signer)?.signTransaction) {
-    return isV6EthersProvider(provider.provider);
   } else if (provider?.provider && provider?.signTransaction) {
     // } else if ((provider as ethersV6.Signer)?.provider && (provider as ethersV6.Signer)?.signTransaction) {
     return isV6EthersProvider(provider.provider);
+    // } else if (provider?._isSigner === true && provider?.provider && provider?.signTransaction) {
+    //   // } else if ((provider as ethers.Signer)._isSigner === true && (provider as ethers.Signer)?.provider && (provider as ethers.Signer)?.signTransaction) {
+    //   return isV6EthersProvider(provider.provider);
   } else if (provider?.provider && !provider?.signTransaction) {
     // } else if ((provider as ethersV6.Provider)?.provider && (provider as ethersV6.Provider)?.signTransaction) {
     return true;
