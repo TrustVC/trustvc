2c7ff2fe7687809fa6b777a1e479e47076a01345
Rishabh Singh <rishabh7singh5@gmail.com>
2025-08-14 13:18:44 +0530

    fix: update edsa w3c verifier

--- src/verify/verify.ts
+++ src/verify/verify.ts
@@ -31,6 +31,7 @@ import type {
   VerificationFragmentWithData,
 } from '@tradetrust-tt/tt-verify/dist/types/src/types/core';
 import { w3cSignatureIntegrity } from './fragments/document-integrity/w3cSignatureIntegrity';
+import { ecdsaW3CSignatureIntegrity } from './fragments/document-integrity/ecdsaW3CSignatureIntegrity';
 import { credentialStatusTransferableRecordVerifier } from './fragments/document-status/transferableRecords/transferableRecordVerifier';
 import { w3cCredentialStatus } from './fragments/document-status/w3cCredentialStatus';
 import { w3cIssuerIdentity } from './fragments/issuer-identity/w3cIssuerIdentity';
@@ -64,6 +65,7 @@ const openAttestationVerifiers = [
 
 const w3cVerifiers: Verifier<VerificationFragment>[] = [
   w3cSignatureIntegrity,
+  ecdsaW3CSignatureIntegrity,
   w3cCredentialStatus,
   credentialStatusTransferableRecordVerifier,
   w3cEmptyCredentialStatus,
