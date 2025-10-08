f48d516a6e5d93af52b04bd624beabf843867370
moiz-sgtradex <moiz.shaikh@sgtradextech.com>
2025-10-08 09:15:59 +0530

    feat: readme update

--- src/__tests__/w3c/derive.test.ts
+++ src/__tests__/w3c/derive.test.ts
@@ -7,7 +7,7 @@ import { deriveW3C } from 'src/w3c';
 import { SignedVerifiableCredential } from '@trustvc/w3c-vc';
 
 describe('W3C derive', () => {
-  // credentialStatus is defined since the document has been signed with credentailStatus as mandatory parameter
+  // credentialStatus is defined since the document has been signed with credentialStatus as mandatory parameter
   it('should derive a W3C v2.0 document using ECDSA-SD-2023 without custom selective pointers', async () => {
     const result = await deriveW3C(
       ECDSA_W3C_VERIFIABLE_DOCUMENT_V2_0 as SignedVerifiableCredential,
