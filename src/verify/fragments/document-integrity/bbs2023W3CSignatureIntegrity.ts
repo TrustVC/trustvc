9bf7c7f825c8dd5cf924aec0c11dbd88009b5891
Moiz Shaikh <58319530+Moiz47@users.noreply.github.com>
2025-10-09 12:42:58 +0530

    feat: bbs2023 verification fragment (#115)
    * feat: bbs2023 verification fragment
    
    * feat: import fix
    
    * feat: bbs 2023 fragment optimization
    
    ---------
    
    Co-authored-by: moiz-sgtradex <moiz.shaikh@sgtradextech.com>

--- src/verify/fragments/document-integrity/bbs2023W3CSignatureIntegrity.ts
+++ src/verify/fragments/document-integrity/bbs2023W3CSignatureIntegrity.ts
@@ -1,9 +1,9 @@
 import { VerificationFragment, Verifier } from '@tradetrust-tt/tt-verify';
 import { createW3CSignatureIntegrityVerifier } from './w3cModernSignatureIntegrityFactory';
 
-export const ecdsaW3CSignatureIntegrity: Verifier<VerificationFragment> =
+export const bbs2023W3CSignatureIntegrity: Verifier<VerificationFragment> =
   createW3CSignatureIntegrityVerifier({
-    cryptosuite: 'ecdsa-sd-2023',
-    name: 'EcdsaW3CSignatureIntegrity',
+    cryptosuite: 'bbs-2023',
+    name: 'Bbs2023W3CSignatureIntegrity',
     derivationPaths: [],
   });
